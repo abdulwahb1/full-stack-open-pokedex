038d9240556a6b7768be8f2e2806f3e1e636bd8b
Abdul Wahab <abdulwabby@gmail.com>
2024-11-01 09:42:06 +0500

    fixed all the errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    indent: ["error", 2],
-    linebreakstyle: ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": "error",
-    "react/prop-types": 0,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'error',
+    'react/prop-types': 0,
   },
-};
+  {
+    "env": {
+      "jest": true // This enables Jest global variables
+    }
+  },
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+const mapResults = ({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1], 10),
+  }))
 
 const App = () => {
-  const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const { name } = useParams()
+  const {
+    data: pokemonList,
+    error,
+    isLoading,
+  } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
   if (isLoading) {
     return <LoadingSpinner />
@@ -26,19 +31,28 @@ const App = () => {
   let next = null
   let previous = null
 
-  if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
+  if (name) {
+    const pokemonId = pokemonList.find(({ name: pokemonName }) => pokemonName === name)?.id
+    previous = pokemonList.find(({ id }) => id === pokemonId - 1) || null
+    next = pokemonList.find(({ id }) => id === pokemonId + 1) || null
   }
 
   return (
-    <Routes>
-      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+        <Route
+          path="/pokemon/:name"
+          element={
+            <PokemonPage
+              pokemonList={pokemonList}
+              previous={previous}
+              next={next}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   )
 }
 
